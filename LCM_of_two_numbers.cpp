d524d5867cd2bdd42f7a98015dc15ae6ae2b9a3c
Kshitiz Bhadra <kshitizbhadra811@gmail.com>
2026-07-02 11:03:49 +0530

    Add LCM_of_two_numbers.cpp and check_leap_year.cpp

---
+++ LCM_of_two_numbers.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a, b;
+  cin >> a >> b;
+  int x = a;
+  int y = b;
+  while (y != 0) {
+    int temp = y;
+    y = x % y;
+    x = temp;
+  }
+  int lcm = (a * b) / x;
+  cout << lcm;
+}
+/* iske ek trick h gcd nikalo and phir lcm= (a*b)/gcd krdo*/
